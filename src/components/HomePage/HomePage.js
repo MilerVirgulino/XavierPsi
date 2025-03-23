46706c101ed72c450693a01a02e52110cb5fb8f5
SrBaonzinho <miler.virgulino@gmail.com>
2025-03-23 18:58:13 -0300

    adcionando menu

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,24 @@
 import React from "react";
+import imgTest from "../../images/test-tube-svgrepo-com.png";
+import './HomePage.css';
 
 const HomePage = ()=>{
+    const handleTouch = ()=>{
+        alert('Touch Event Detected');
+    }
     return (
+        <div className="container">
+
         <div>
-            <h1>Welcome to My React App!</h1>
-            <p>This is the home page.</p>
+            <img src={imgTest} onClick={handleTouch}/>
+            <img src={imgTest} onClick={handleTouch}/>
+            <img src={imgTest} onClick={handleTouch}/>
+        </div>
+        <div>
+            <img src={imgTest} onClick={handleTouch}/>
+            <img src={imgTest} onClick={handleTouch}/>
+            <img src={imgTest} onClick={handleTouch}/>
+        </div>
         </div>
     )
 }
